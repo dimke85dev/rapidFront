e8942f347b792ea73dc1fccc838637f03ae8aea6
Dimke85dev <dimke85.dev@gmail.com>
2023-04-12 19:36:00 +0300

    for versel

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instanse = axios.create({
-  // baseURL: 'https://rapid-back-6fsu0u79q-dimke85dev.vercel.app/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://rapid-back.vercel.app/api',
+  // baseURL: 'http://localhost:5000/api',
 });
 
 instanse.interceptors.request.use((config) => {
