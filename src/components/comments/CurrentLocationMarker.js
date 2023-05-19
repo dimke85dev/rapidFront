8c127cd63dffe5dd06e2fb0cc7d19fe12849e043
Dimke85dev <dimke85.dev@gmail.com>
2023-05-19 17:27:30 +0300

    v 1.1.7 ContactForm GoogleMaps

---
+++ src/components/comments/CurrentLocationMarker.js
@@ -0,0 +1,39 @@
+import { Marker } from '@react-google-maps/api';
+import React, { Fragment } from 'react';
+import styles from './CurrentLocationMarker.module.css';
+
+const CurrentLocationMarker = ({ position }) => {
+  const handleMarkerClick = () => {
+    const { lat, lng } = position;
+
+    const isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    if (isMobile) {
+      const url = `geo:${lat},${lng}`;
+      window.location.href = url;
+    } else {
+      // Открытие Google Карт в новой вкладке и построение маршрута
+      const url = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+      window.open(url);
+    }
+  };
+
+  return (
+    <Fragment>
+      <Marker
+        onClick={handleMarkerClick}
+        position={position}
+        animation={window.google.maps.Animation.BOUNCE}
+        title={'RapidService'}
+        icon={{
+          url: '../../../check.svg',
+        }}
+      />
+    </Fragment>
+  );
+};
+
+export default CurrentLocationMarker;
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,42 +1,87 @@
-import React, { Fragment } from 'react';
+import React, {
+  Fragment,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { useMemo } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 import styles from './Map.module.css';
 import Loader from '../components/UI/Loader';
+import { defaultTheme, newYorkTheme } from './ThemeMap';
+import CurrentLocationMarker from '../components/comments/CurrentLocationMarker';
+
+const defaultOptionsMap = {
+  panControl: true,
+  zoomControl: true,
+  mapTypeControl: false,
+  scaleControl: false,
+  streetViewControl: false,
+  rotateControl: false,
+  clickableIcons: false,
+  keyboardShortcuts: false,
+  scrollwheel: true,
+  disableDoubleClickZoom: true,
+  fullscreenControl: false,
+  styles: newYorkTheme,
+};
+
+const libraries = ['places'];
 
 const Map = () => {
+  const mapRef = useRef(undefined);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const zoom = width < 768 ? 15 : 16;
+
+  const onLoad = useCallback(function callback(map) {
+    mapRef.current = map;
+  }, []);
+  const onUnmount = useCallback(function callback(map) {
+    mapRef.current = undefined;
+  }, []);
+
   const center = useMemo(
     () => ({
-      lat: 49.82413102441198,
-      lng: 23.982057654910328,
+      lat: 49.82413,
+      lng: 23.982449,
     }),
     []
   );
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
   });
 
-  const handleMarkerClick = (event) => {
-    console.log(1);
-  };
+  const handleMarkerClick = (event) => {};
 
   if (!isLoaded) return <Loader />;
 
   return (
     <Fragment>
       <GoogleMap
-        zoom={16}
+        options={defaultOptionsMap}
+        zoom={zoom}
         center={center}
         mapContainerClassName={`${styles.mapcontainer}`}
+        libraries={libraries}
       >
-        <Marker
+        <CurrentLocationMarker position={center} />
+        {/* <Marker
           title={'RapidService'}
           onClick={handleMarkerClick}
           position={{
             lat: 49.82413102441198,
             lng: 23.982057654910328,
           }}
-        ></Marker>
+        ></Marker> */}
       </GoogleMap>
     </Fragment>
   );
