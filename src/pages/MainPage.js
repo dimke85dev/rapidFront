ddb9e5269b5d04ff91638061800eafa1c95a74e2
Dimke85dev <dimke85.dev@gmail.com>
2023-07-11 20:07:32 +0300

    captca add

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -33,7 +33,7 @@ const MainPage = () => {
         <Loader />
       ) : (
         <>
-        <Turnstile siteKey='1x00000000000000000000AA' />
+        <Turnstile siteKey='0337dc2af1a990ca89b95c2cd63fb35ee1547' />
         <div className="max-w-[900px] bg-slate-200 mx-auto py-10 px-3">
           <div className="post-elem flex justify-between gap-8">
             <div className="flex flex-col gap-10 basis-4/5">
